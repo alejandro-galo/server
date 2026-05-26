a0f68fe53f69efd5478734c0f886e1c275eb2827
Gonzalo Alejandro Tintaya Farfan <202300889h@utea.edu.pe>
2026-05-25 19:06:47 -0500

    update

--- tests/Feature/UpdateEventTest.php
+++ tests/Feature/UpdateEventTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Event;
+use Carbon\Carbon;
 
 class UpdateEventTest extends TestCase
 {
@@ -12,13 +14,31 @@ class UpdateEventTest extends TestCase
     /**
      * A basic feature test example.
      */
+    protected $event;
+    public function setup(): void
+    {
+        parent::setUp();
+        $this->event = event::create([
+            'name' => 'Conferencia de YouDevs',
+            'featured' => 'meme.png',
+            'date' => Carbon::now()->format('Y-m-d'),
+            'time' => '12:00:00',
+            'location' => 'EL SANTIAGO BERNABEU',
+        ]);
+    }
     public function test_example(): void
     {
         //Arrange
-        //Act
-        //Assert
-        $response = $this->get('/');
+        $updatedData = [
+            'name' => 'Evento actualizado',
+        ];
 
+         
+        //Act
+        $response = $this->put('/events/'. $this->event->id, $updatedData);
+        
+        //Assert
         $response->assertStatus(200);
+        $this->assertDatabaseHas('events', $updatedData);
     }
 }
